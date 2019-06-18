e78099cda24e6974ac89cca379e31600fcedb4e3
Graves <theredhead242@gmail.com>
2019-06-18 16:01:41 -0700

    updated from string based representation system to object based

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os
 
 #global vars
 
-
+#Sets up and manages the animals, foods, grid, and overall game.
 class Environment:
   def __init__(self,h,w, numAnimals):
     self.h = h
@@ -14,54 +14,29 @@ class Environment:
     self.animals = []
     self.foods = []
     
+    #initilization
     for i in range (numAnimals):  
-      an1 = Animal("@", [0,0])
+      an1 = Animal("@", [4,4])
       self.animals.append(an1)
       
     self.foodPerDay = int((self.h * self.w) / 50)
     for i in range(self.foodPerDay):
       food1 = Food("#", [random.randint(0, self.h -1 ), random.randint(0, self.w - 1)])
-      self.grid[food1.location[0]][food1.location[1]] = food1.char
-      
+      self.grid[food1.location[0]][food1.location[1]].append(food1)
       self.foods.append(food1)   
       
        
-  
-      
-
   def update(self):
     for animal in self.animals:
+      #we dont know where the animal was after it updates
 
-
-      if(type(self.grid[animal.location[0]][animal.location[1]]) == "Food"):
-        
-
-      
-      self.grid[animal.location[0]][animal.location[1]] = ","
-      #this is where the code for the animals eating food will go
-
-
-      #remove the food we run over from the food array 
-
-
-
-      for x in range(animal.location[0] - 1, animal.location[0] + 1):
-        for y in range (animal.location[1] - 1, animal.location[1] + 1):
-          if type(self.grid[x][y]) == "Food":
-            if self.grid[x][y].objType == "food":
-              print("found food")
-
-
-
-
-
-
-
-
-
-
+      #take the animal at its old location and pop it out of the tile 
+      #this is the tile that the animal is in 
+      self.grid[animal.location[0]][animal.location[1]].pop(-1)
+     
       animal.update()
-      self.grid[animal.location[0]][animal.location[1]] = animal.char
+      #at the new tile, append the animal
+      self.grid[animal.location[0]][animal.location[1]].append(animal)
     self.displayEnv()
 
   def createGrid(self,h,w):
@@ -69,14 +44,28 @@ class Environment:
     for i in range(h):
       row = []
       for j in range(w):
-        row. append(",")
+        tile = []
+        #create a terrain object at i and j, and then place it into tile 
+        ter1 = Terrain(i, j, ",")
+        tile.append(ter1)
+        row.append(tile)
       grid.append(row)
     return grid
+
   def displayEnv(self):
     for i in range(self.h):
       for j in range(self.w):
-        print(self.grid[i][j], end = " ")
+        #printing the string rn 
+        
+        print(self.grid[i][j][-1].char, end = " ")
       print("")
+
+
+class Terrain:
+  def __init__(self, x, y, char):
+    self.x = x
+    self.y = y
+    self.char = char
 
 class Animal:
   def __init__(self, char, location):
@@ -84,7 +73,7 @@ class Animal:
     self.location = location
     self.eaten = 0
     self.objType = "animal"
-
+  #changes an instance var of animal, not its actual position
   def move(self):
     self.location[0] += random.randint(-1, 1)
     self.location[1] += random.randint(-1, 1)
@@ -110,19 +99,12 @@ class Food:
     if self.nutrition <= 0:
       del self
 
-      
-      
-    
-
-    
-
-
 env1 = Environment(20,20,6)
 simTime = 120
 
 while(simTime > 0):
   env1.update()
-  time.sleep(0.1)
+  time.sleep(1)
  
   os.system('clear')
   simTime-=1
